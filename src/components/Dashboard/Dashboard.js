30dd44e0ae2924508b7baf375f7653d11c44f477
cami98735264 <cr7montealegre2010@gmail.com>
2024-04-28 22:44:19 -0500

    Minor bug fixed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,23 @@
 
 import loginContext from "../../index.js";
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
 import LoginHandler from "../../utils/LoginHandler/LoginHandler.js";
 
 
 const Dashboard = () => {
     const userContext = useContext(loginContext);
-    const { email, id, lastname, names, second_lastname, telefono } = userContext.userData.data;
+    const [data, setUserData] = useState({});
+    useEffect(() => {
+        if(userContext.requestCompleted && userContext.isLogged) {
+            setUserData(userContext.userData.data);
+        }
+    }, [userContext.requestCompleted])
     console.log(userContext);
     return (
         <LoginHandler>
             <div>
-                <h1 className="text-center my-5">#{id} {"->"} ¡Bienvenido {email} ~ {telefono} | <i>{names} {lastname} {second_lastname}</i></h1>
+                <h1 className="text-center my-5"><i>{data.length === 0 ? "Cargando..." : `#${data.id} {"->"} ¡Bienvenido ${data.email} ~ ${data.telefono} | ${data.names} ${data.lastname} ${data.second_lastname}`}</i></h1>
             </div>
         </LoginHandler>
     );
